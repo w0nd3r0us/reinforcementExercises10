245c27fadc6993509de570ff12317caecb296b3b
w0nd3r0us <steven.grewal@gmail.com>
2018-06-27 00:00:00 -0400

    completed assignement

---
+++ room.rb
@@ -0,0 +1,32 @@
+hotel = { data: { rooms:
+    [ { id: 1, room_number: "201", capacity: 50}, { id: 2, room_number: "301", capacity: 200 }, { id: 3, room_number: "1B", capacity: 100}
+    ],
+    events: [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+#1 Retrieve the capacity of room 201 and store it in a variable.
+puts capacity_201 = hotel[:data][:rooms][0][:capacity]
+
+#2 Find all the events taking place in room 201. Iterate through them and print "ok" if the number of planned attendees will fit in the room.
+
+id = 0
+hotel[:data][:rooms].each do |room|
+  if room[:room_number] == "201"
+    id = room[:id]
+  end
+end
+hotel[:data][:events].each do |event|
+  if event[:room_id] == id
+    if event[:attendees] > hotel[:data][:rooms][0][:capacity]
+      p "Over capacity!"
+    else
+      p 'OK!'
+    end
+  end
+end
